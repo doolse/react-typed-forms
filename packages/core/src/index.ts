53fd002e1766748ed4b3490597d23c2975bf55d9
Jolse Maginnis <doolse@gmail.com>
2024-10-29 17:27:48 +1100

    Detect the version

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -4,3 +4,11 @@ export * from "./html";
 export * from "./types";
 export * from "./components";
 export * from "./util";
+
+const version = "3.6.2";
+const existingVersion = (globalThis as any)["_react_typed_forms"]; 
+if (existingVersion !== "undefined") {
+    console.warn(`Multiple versions of @react-typed-forms/core found: ${existingVersion} and ${version}`);
+}
+(globalThis as any)["typedForms"] = version;
+console.info("@react-typed-forms/core "+version+" loaded");
